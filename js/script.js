465596e47a62629998757e5e7bb95a8384212ed2
Dipta Paul <diptapaul.edu@gmail.com>
2026-07-20 15:46:27 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-// FrontierX Lab JavaScript
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections(){
+
+    reveals.forEach(section => {
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = section.getBoundingClientRect().top;
+
+        const revealPoint = 120;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealSections);
+
+window.addEventListener("load", revealSections);
